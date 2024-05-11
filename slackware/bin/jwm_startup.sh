9545d9f14a9071c1848fb158dc4d9f3e101b81e3
aktsbot <box.ashishk@gmail.com>
2024-05-11 20:35:28 +0530

    add more slackware configs
    jwm :)

--- slackware/bin/jwm_startup.sh
+++ slackware/bin/jwm_startup.sh
@@ -7,14 +7,17 @@ xset r rate 350 50
 xsetroot -cursor_name left_ptr
 /usr/lib64/xfce4/notifyd/xfce4-notifyd &
 /usr/libexec/polkit-gnome-authentication-agent-1 &
+#(sleep 1s && pcmanfm --desktop) &
+thunar --daemon &
+nitrogen --restore &
 nm-applet &
 redshift &
-pcmanfm -d &
+#pcmanfm -d &
 start_pulseaudio.sh &
 (sleep 1s && volumeicon) &
 compton &
 xfce4-clipman &
-(sleep 1s && pcmanfm --desktop) &
 blueman-applet &
+(sleep 1s && idesk) &
 (sleep 1s && conky -c ~/.config/conky/conkyrc.lua) &
 
